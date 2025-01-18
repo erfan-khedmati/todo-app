cb6a2ba5ee9c4c300f8e3b9036261193fc2b0cc4
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2025-01-18 19:37:44 +0330

    install depenencies

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],  // Add .jsx here
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Update this to handle both .js and .jsx
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    port: 5000,
+    hot: true,
+  },
+};
