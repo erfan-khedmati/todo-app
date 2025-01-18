11ba09510da14fe7c0942214a4b67bf446b36a0e
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2025-01-19 01:42:23 +0330

    add navabar and frame buttons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./navbar.scss"
+import CloseIcon from '@mui/icons-material/Close';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="frame">
+      {/* Fram Action */}
+      <div className='frameActions'>
+        <button className='frameButton'>
+          <RemoveIcon className="icon"/>
+        </button>
+        <button className='frameButton'>
+          <CloseIcon className="icon"/>
+        </button>
+      </div>
+    </div>
   )
 }
 
