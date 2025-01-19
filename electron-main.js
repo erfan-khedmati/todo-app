f555f0e56054122e4f6ae4c87c5332b71375ea35
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2025-01-19 15:01:24 +0330

    add frame button functionallity

--- electron-main.js
+++ electron-main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, screen } = require('electron');
+const { app, BrowserWindow, screen, ipcMain } = require('electron');
 const path = require('path');
 
 let mainWindow;
@@ -6,13 +6,15 @@ let mainWindow;
 app.on('ready', () => {
 
   // Get the primary display size
-  const {width, height} = screen.getPrimaryDisplay().workAreaSize;
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
   mainWindow = new BrowserWindow({
     width: width,
     height: height,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
     },
 
     frame: false // hide frame
@@ -22,5 +24,9 @@ app.on('ready', () => {
   mainWindow.setMenuBarVisibility(false)
 
   // mainWindow.loadFile(path.join(__dirname, 'dist', 'index.html'));
-    mainWindow.loadURL("http://localhost:5000/")
+  mainWindow.loadURL("http://localhost:5000/")
+  
+  ipcMain.on("minimizeWindow", () => {
+    mainWindow.minimize();
+  })
 });
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "./navbar.scss"
 import CloseIcon from '@mui/icons-material/Close';
 import RemoveIcon from '@mui/icons-material/Remove';
@@ -7,6 +8,16 @@ import RemoveIcon from '@mui/icons-material/Remove';
 import Profiledropdown from "../profiledropdown/Profiledropdown";
 
 function Navbar() {
+
+  // Window Actions
+  function closeWindow() {
+    window.close();
+  }
+
+  function minimizeWindow() {
+    window.electronAPI.min()
+  }
+
   return (
     <div className="frame">
       {/* Drag Area */}
@@ -15,10 +26,10 @@ function Navbar() {
       <Profiledropdown username="efdeveloper" />
       {/* Fram Action */}
       <div className='frameActions'>
-        <button className='frameButton'>
+        <button className='frameButton' onClick={minimizeWindow}>
           <RemoveIcon className="icon"/>
         </button>
-        <button className='frameButton'>
+        <button className='frameButton' onClick={closeWindow}>
           <CloseIcon className="icon"/>
         </button>
       </div>
