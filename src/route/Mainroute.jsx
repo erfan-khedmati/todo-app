af78ab5a193225e686fcfca662f89eea0355ed65
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2025-01-21 14:43:09 +0330

    create the hash able rout and create task page id rout

--- src/route/Mainroute.jsx
+++ src/route/Mainroute.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 
 // Component
 import Mainlayout from "../layout/Manlayout"
 import Home from "../pages/home/Home";
 import Profile from "../pages/profile/Profile";
+import Task from "../components/Task/Task";
 
 function Mainroute() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Mainlayout />}>
           <Route index element={<Home />} />
+          <Route path="task/:taskID" element={<Task />}>
+            
+          </Route>
           <Route path="profile" element={<Profile />} />
         </Route>
         <Route path="*" element={<div>404</div>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
