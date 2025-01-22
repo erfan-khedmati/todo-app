363153d5305937edb376a11738dc011492d7276b
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2025-01-23 01:25:02 +0330

    add edit button(usage in next commit)

---
+++ src/components/editbutton/Editbutton.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// Scss file
+import "./editbutton.scss"
+
+// Icon
+import EditIcon from '@mui/icons-material/Edit';
+
+function Editbutton() {
+    // function handleChangeIcon() {
+        
+    // }
+  return (
+    <button className='edit'>
+        <EditIcon className='icon' />
+    </button>
+  )
+}
+
+export default Editbutton
