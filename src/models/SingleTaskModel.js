6f7407e0e38de8dc452e3c8683f193e9d2fb4a61
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2025-01-25 16:57:56 +0330

    create single task model

---
+++ src/models/SingleTaskModel.js
@@ -0,0 +1,32 @@
+export default class SingleTaskModel {
+    constructor(id, title, description, star, addedDate, limit_time) {
+        this.id = id;
+        this.title = title
+        this.description = description;
+        this.star = star;
+        this.addedDate = addedDate;
+        this.limit_time = limit_time;
+    }
+
+    static createFormData(data, taskID) {
+        const task = data.find(t => t.id.toString() === taskID);
+
+        return new SingleTaskModel(
+            task.id,
+            task.title,
+            task.description,
+            task.star,
+            task.added_date,
+            task.limit_time
+        )
+    }
+
+    updateData(title, description, limit_time, star) {
+        const isTitleChanged = this.title !== title ? title : null
+        const isDeScriptionChanged = this.description !== description ? description : null
+        const isLimitDateChanged = this.limit_time !== limit_time ? limit_time : null
+        const isStarChanged = this.star !== star ? star : null
+        // Star is remaining will fixed
+        window.electronAPI.writeTask("./src/data/data.json", this.id,isTitleChanged, isDeScriptionChanged, isLimitDateChanged, isStarChanged)
+    }
+}
