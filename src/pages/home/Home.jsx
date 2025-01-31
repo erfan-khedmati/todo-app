50100819c39d48ee5bb712e52e3399d85d632886
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2025-01-31 15:41:39 +0330

    create add task in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import "./home.scss"
+import { Link } from "react-router-dom";
+
+import "./home.scss";
 
 // Components
-import Startlist from "../../components/starlist/Starlist"
+import Startlist from "../../components/starlist/Starlist";
 
 function Home() {
   return (
-    <div className='home'>
+    <div className="home">
+      {/* Add Task */}
+      <div className="add_task">
+        <h1 className="title">Add Taks</h1>
+        <Link to="/add-task" className="link">+</Link>
+      </div>
+      {/* Star List */}
       <Startlist />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
